172be6fab6ffa8baac40c4d75a5a2af71ad40724
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-24 22:35:56 -0400

    setting up firebase for auth

--- my-authapp/src/App.js
+++ my-authapp/src/App.js
@@ -1,6 +1,25 @@
 import './App.css';
+import fire from "./fire.js";
+import {useState,useEffect} from 'react';
 
 function App() {
+  const [users,setUsers] = useState({})
+
+  useEffect(() => {
+    authListener();
+    },[]);
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        setUsers(user );
+      } else {
+        setUsers({ user: null });
+      }
+    });
+  }
+
   return (
     <div className="App">
       <h1>hello</h1>
---
+++ my-authapp/src/Login.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+
+
+
+
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
