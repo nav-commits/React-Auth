fc6b5eb46739b3ca87e1fcd6ebb3bb776ea3f892
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-24 22:40:19 -0400

    added login and signup

--- my-authapp/src/Login.js
+++ my-authapp/src/Login.js
@@ -1,17 +1,62 @@
 import './App.css';
 import {useState} from 'react';
+import fire from "./fire.js";
 
 function App() {
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
 
+  login = e => {
+    e.preventDefault();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email,password)
+      .then(u => {
+        console.log(u);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
+
+  signup = e => {
+    e.preventDefault();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email,password)
+      .then(u => {
+        console.log(u);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
 
 
   return (
     <div className="App">
-      <h1>hello</h1>
+          <form>
+          <input
+            name="email"
+            type="email"
+            id="email"
+            placeholder="enter email"
+            onChange={handleChange}
+            value={email}
+          />
+          <input
+            name="password"
+            type="password"
+            id="password"
+            placeholder="enter password"
+            onChange={handleChange}
+            value={password}
+          />
+          <button onClick={login}>Login </button>
+          <button onClick={signup}>Sign Up </button>
+        </form>
     </div>
   );
 }
